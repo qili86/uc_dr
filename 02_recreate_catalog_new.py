d603e1e23ce0094eb73120bdb2bb2d383280edf2
qili86 <qili86>
2024-02-02 06:24:21 +0000

    copy table properties etc

--- 02_recreate_catalog_new.py
+++ 02_recreate_catalog_new.py
@@ -14,7 +14,7 @@
 # COMMAND ----------
 
 dbutils.widgets.removeAll()
-dbutils.widgets.text("storageLocation", "/mnt/externallocation", "Storage with source catalog info")
+dbutils.widgets.text("storageLocation", "abfss://metadata@eastus2external.dfs.core.windows.net/", "Storage with source catalog info")
 dbutils.widgets.text("rootExternalStorage", "abfss://data@starbucksdev.dfs.core.windows.net/daibo/", "Root of external tables' path")
 
 # COMMAND ----------
@@ -52,8 +52,19 @@ def return_schema(df):
 
 # COMMAND ----------
 
+tables_details_df = spark.read.format("delta").load(f"{storage_location}/uc_dr_tables_details")
+
+# COMMAND ----------
+
+display(tables_details_df)
+
+# COMMAND ----------
+
 catalogs_df = spark.read.format("delta").load(f"{storage_location}/uc_dr_catalogs")
-table_errors = []
+tables_details_df = spark.read.format("delta").load(f"{storage_location}/uc_dr_tables_details")
+table_errors = [["dummy_table", "dummy_error"]]
+table_properities_system_level = ['delta.liquid.clusteringColumns', 'delta.rowTracking.materializedRowIdColumnName', 'delta.rowTracking.materializedRowCommitVersionColumnName']
+
 
 for catalog in catalogs_df.collect():
     catalog_name = catalog.catalog
@@ -66,7 +77,6 @@ for catalog in catalogs_df.collect():
 
     #Get only user schemas
     schemas_df = spark.read.format("delta").load(f"{storage_location}/{catalog_name}/schemata").filter("schema_name<>'information_schema'")
-    display(schemas_df)
 
     #Drop the default schema
     spark.sql(f"DROP SCHEMA {catalog_name}.default CASCADE")
@@ -76,12 +86,10 @@ for catalog in catalogs_df.collect():
         spark.sql(f"CREATE SCHEMA IF NOT EXISTS {catalog_name}.{schema.schema_name} COMMENT '{schema.comment}'")
         spark.sql(f"ALTER SCHEMA {catalog_name}.{schema.schema_name} SET OWNER to `{schema.schema_owner}`")
 
-
     #Get only external user tables
     tables_df = spark.read.format("delta").load(f"{storage_location}/{catalog_name}/tables").filter("table_schema<>'information_schema' and table_type='EXTERNAL'")
     tables_df = tables_df.withColumn("full_name", concat_ws(".", col("table_catalog"), col("table_schema"), col("table_name")))
-    table_location_df = spark.read.format("delta").load(f"{storage_location}/uc_dr_table_locations")
-    tables_df = tables_df.join(table_location_df, on="full_name", how="left")
+    tables_df = tables_df.join(tables_details_df, on="full_name", how="left")
 
     for table in tables_df.collect():
         columns_df = spark.read.format("delta").load(f"{storage_location}/{catalog_name}/columns").filter((col("table_schema") == table.table_schema) & (col("table_name") == table.table_name))
@@ -90,10 +98,29 @@ for catalog in catalogs_df.collect():
         #Create Table
         # spark.sql(f"CREATE OR REPLACE TABLE {catalog_name}.{table.table_schema}.{table.table_name}({columns}) COMMENT '{table.comment}' LOCATION '{root_externalstorage}{table.table_schema}/{table.table_name}'")
         try:
-            spark.sql(f"CREATE OR REPLACE TABLE {catalog_name}.{table.table_schema}.{table.table_name}({columns}) COMMENT '{table.comment}' LOCATION '{table.location}'")
+            ct_st = f"CREATE OR REPLACE TABLE {catalog_name}.{table.table_schema}.{table.table_name}({columns}) COMMENT '{table.comment}' LOCATION '{table.location}'"
+            if table.partitionColumns != []:
+                pks = ",".join(table.partitionColumns)
+                ct_st = ct_st + f" PARTITIONED BY ({pks})" 
+            if table.clusteringColumns != []:
+                cks = ",".join(table.clusteringColumns)
+                ct_st = ct_st + f" CLUSTER BY ({cks})" 
+            if table.properties != {}:
+                ppts=table.properties
+                for ppt in table_properities_system_level:
+                    ppts.pop(ppt, None)
+                if ppts != {}:
+                    properties_str = "" 
+                    for key, value in ppts.items():
+                        properties_str=properties_str+ f"'{key}'='{value}',"
+                    ct_st = ct_st + f" TBLPROPERTIES ({properties_str[:-1]})"
+            print(ct_st)
+            spark.sql(ct_st)
             spark.sql(f"ALTER TABLE {catalog_name}.{table.table_schema}.{table.table_name} SET OWNER to `{table.table_owner}`")
         except Exception as e:
+            print(f"An unexpected error occurred: {e}")
             table_errors.append([f"{catalog_name}.{table.table_schema}.{table.table_name}", f"An unexpected error occurred: {e}"])
 
 table_errors_df = spark.createDataFrame(table_errors, ['table', 'error'])
-display(table_errors_df)
+if table_errors_df.count() > 1:
+   display(table_errors_df)
