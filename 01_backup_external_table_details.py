29dbaa3ace30617151001545a93631476e5af11f
qili86 <qili86>
2024-02-05 21:59:01 +0000

    refactoring

---
+++ 01_backup_external_table_details.py
@@ -0,0 +1,71 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ##Backup External Table Details
+# MAGIC
+# MAGIC This notebook will read from the information_schema of a given catalog and dump its contents to external storage.
+# MAGIC
+# MAGIC This external storage will be independent from the UC storage and will be accessible by a remote workspace on a different region with a different UC.
+# MAGIC
+# MAGIC Assumptions:
+# MAGIC - Dump and restore one catalog at a time (currently overwrites to the same folder)
+
+# COMMAND ----------
+
+dbutils.widgets.removeAll()
+dbutils.widgets.text("storageLocation", "abfss://system@eastus2external.dfs.core.windows.net/", "Storage location for copy")
+
+# COMMAND ----------
+
+storage_location = dbutils.widgets.get("storageLocation")
+print(storage_location)
+
+# COMMAND ----------
+
+# all_catalogs = spark.read.format("delta").load(f"{storage_location}/catalogs").filter("catalog_owner<>'System user'")
+
+# COMMAND ----------
+
+# display(all_catalogs)
+
+# COMMAND ----------
+
+tables_df = spark.read.format("delta").load(f"{storage_location}/tables").filter("table_schema<>'information_schema' and table_type='EXTERNAL'")
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, ArrayType, MapType
+
+tables_info = []
+table_errors = []
+tables_create_stmt = []
+
+for table in tables_df.collect():
+    print(f"----scan details for external table: {table.table_catalog}.{table.table_schema}.{table.table_name}")
+    try:
+        table_info = spark.sql(f"DESCRIBE DETAIL {table.table_catalog}.{table.table_schema}.{table.table_name}").first()
+        tables_info.append([f"{table.table_catalog}.{table.table_schema}.{table.table_name}", table_info['location'], table_info['partitionColumns'], table_info['clusteringColumns'], table_info['properties'], table_info['format']])
+        createtab_stmt = spark.sql(f"SHOW CREATE TABLE {table.table_catalog}.{table.table_schema}.{table.table_name}").first()['createtab_stmt']
+        tables_create_stmt.append([f"{table.table_catalog}.{table.table_schema}.{table.table_name}", createtab_stmt])
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        table_errors.append([f"{table.table_catalog}.{table.table_schema}.{table.table_name}", f"An unexpected error occurred: {e}"])
+
+schema = StructType([
+    StructField("full_name", StringType(), True),
+    StructField("location", StringType(), True),
+    StructField("partitionColumns", ArrayType(StringType()), True),
+    StructField("clusteringColumns", ArrayType(StringType()), True),
+    StructField("properties", MapType(StringType(), StringType()), True),
+    StructField("format", StringType(), True)
+])
+
+tables_info_df = spark.createDataFrame(tables_info, schema)
+tables_info_df.write.format("delta").mode("overwrite").save(f"{storage_location}/uc_dr_tables_details")
+
+tables_create_stmt_df = spark.createDataFrame(tables_create_stmt, ['name', 'stmt'])
+tables_create_stmt_df.write.format("delta").mode("overwrite").save(f"{storage_location}/uc_dr_tables_create_stmts")
+display(tables_create_stmt_df)
+
+if len(table_errors) > 0:
+    table_errors_df = spark.createDataFrame(table_errors, ['table', 'error'])
+    display(table_errors_df)
